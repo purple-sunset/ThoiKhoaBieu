a43c6c5f3aee60c017164414ffdbe607c9f75987
purple-sunset <purple.sunshine.alone@gmail.com>
2016-12-20 19:29:56 +0700

    Add project files.

---
+++ ThoiKhoaBieu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThoiKhoaBieu.Startup))]
+namespace ThoiKhoaBieu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
